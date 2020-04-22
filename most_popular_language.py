eb7382100444bb61eee89b9be97ebb0955a9c12a
Rahul Patel <patelr4142@gmail.com>
2020-04-22 18:50:00 +0530

    reformat the code

--- most_popular_language.py
+++ most_popular_language.py
@@ -1,18 +1,19 @@
 import csv
-from collections import defaultdict,Counter
-with open('C:\\Users\\patel\\OneDrive\\Desktop\\data\\survey_results_public.csv',encoding='utf-8')as f:
-    total=0
-    csv_reader=csv.DictReader(f)
+from collections import defaultdict, Counter
 
-    language_counter=Counter()
+with open(
+    "C:\\Users\\patel\\OneDrive\\Desktop\\data\\survey_results_public.csv",
+    encoding="utf-8",
+) as f:
+    total = 0
+    csv_reader = csv.DictReader(f)
+    language_counter = Counter()
     for line in csv_reader:
-        languages=line['LanguageWorkedWith'].split(';')
+        languages = line["LanguageWorkedWith"].split(";")
         language_counter.update(languages)
-        total+=1
-
-print('-----Most Popular Technologies-----')
-for language,value in language_counter.most_common(5):
-    language_pct=(value/total)*100
-    language_pct=round(language_pct,2)
+        total += 1
+print("-----Most Popular Technologies-----")
+for language, value in language_counter.most_common(5):
+    language_pct = (value / total) * 100
+    language_pct = round(language_pct, 2)
     print(f"{language}:{language_pct}%")
-                
